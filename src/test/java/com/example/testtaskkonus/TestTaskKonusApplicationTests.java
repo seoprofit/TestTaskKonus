3a1dedfc25232597f10072b2649b8164636e6f97
Kirill Vorobyev <seoprofit@mail.ru>
2024-06-02 20:45:42 +0400

    Refactor test class

--- src/test/java/com/example/testtaskkonus/TestTaskKonusApplicationTests.java
+++ src/test/java/com/example/testtaskkonus/TestTaskKonusApplicationTests.java
@@ -1,11 +1,18 @@
 package com.example.testtaskkonus;
 
+import com.example.testtaskkonus.entity.AuthorEntity;
+import com.example.testtaskkonus.entity.BookEntity;
+import com.example.testtaskkonus.repository.AuthorRepository;
+import com.example.testtaskkonus.repository.BookRepository;
 import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -17,28 +24,50 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class TestTaskKonusApplicationTests {
 
-	@Autowired
-	private MockMvc mockMvc;
-	@Test
-	public void getAllBooks() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders
-						.get("/api/books")
-						.accept(MediaType.APPLICATION_JSON))
-				.andDo(print())
-				.andExpect(status().isOk())
-				.andExpect(MockMvcResultMatchers.jsonPath("$.[*].ID").isNotEmpty());
-	}
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private BookRepository bookRepository;
+    @Autowired
+    private AuthorRepository authorRepository;
 
-	@Test
-	public void getAllAuthors() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders
-						.get("/api/authors")
-						.accept(MediaType.APPLICATION_JSON))
-				.andDo(print())
-				.andExpect(status().isOk())
-				.andExpect(MockMvcResultMatchers.jsonPath("$.[*].ID").isNotEmpty());
-	}
+    @BeforeEach
+    public void addBook() {
+        BookEntity bookEntity = new BookEntity();
+        bookEntity.setName("Book 1");
+        bookEntity.setISBN(1242141);
+        bookRepository.save(bookEntity);
 
+    }
+
+    @BeforeEach
+    public void addAuthor() {
+        AuthorEntity authorEntity = new AuthorEntity();
+        authorEntity.setName("Book 1");
+        authorRepository.save(authorEntity);
+
+    }
+
+
+    @Test
+    public void getAllBooks() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/books")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[*].ID").isNotEmpty());
+    }
+
+    @Test
+    public void getAllAuthors() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/authors")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[*].ID").isNotEmpty());
+    }
 
 
 }
